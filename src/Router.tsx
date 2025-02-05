4075e782512e0de4ebb3943ea3fd50ed910e4f47
marettialine <alinesmaretti@gmail.com>
2025-02-05 17:33:27 -0300

    feat(Router): add MainGallery page route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
+import { MainGallery } from './pages/MainGallery'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route index element={<Home />} />
+        <Route path="galeria" element={<MainGallery />} />
       </Route>
     </Routes>
   )
