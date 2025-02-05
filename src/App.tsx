95db5495c83bb6943afbf5aa80e4f79ceb83ba3f
marettialine <alinesmaretti@gmail.com>
2025-02-05 07:58:11 -0300

    feat: set up routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+
 export function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
