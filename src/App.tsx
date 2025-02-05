07aaaac9925714a67cf5e1a22b4d6333df62eb7f
marettialine <alinesmaretti@gmail.com>
2025-02-05 08:18:31 -0300

    feat(App): add GlobalStyle and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
