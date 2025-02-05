d1f8f43e2180a570eddc4dc69efc4a9cc471498d
marettialine <alinesmaretti@gmail.com>
2025-02-05 08:07:00 -0300

    chore: apply eslint fixes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./Router";
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,5 @@
-import { /*Route,*/ Routes } from "react-router-dom";
+import { /* Route, */ Routes } from 'react-router-dom'
 
-export function Router(){
-    return ( 
-        <Routes>
-            {/* <Route index element={<Home />} /> */}
-        </Routes>
-    )
+export function Router() {
+  return <Routes>{/* <Route index element={<Home />} /> */}</Routes>
 }
