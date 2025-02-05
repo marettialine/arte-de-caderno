0a114ae477cb9a24044e31dd82f21610203fda97
marettialine <alinesmaretti@gmail.com>
2025-02-05 17:41:37 -0300

    feat: add typings for gallery draw data

---
+++ src/@types/draw.ts
@@ -0,0 +1,14 @@
+export interface DrawData {
+  id: string
+  img?: File | null
+  imgLink?: string
+  title: string
+  category: string
+  author: string
+  authorName?: string
+  locale?: string
+  gradeAverage?: number
+  year?: number
+  authorAge?: string
+  school?: string
+}
